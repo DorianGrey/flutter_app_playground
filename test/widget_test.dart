59872b4c4d8034f9d57933b9dc5595d066b33c89
Christian Linne <christian.linne@flavia-it.de>
2019-01-11 14:30:54 +0100

    Attempt to fix test. Add issue ref on GH.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,22 +13,30 @@ import 'package:flutter_app_playground/my_app.dart';
 import 'package:flutter_app_playground/store/state.dart';
 import 'package:flutter_app_playground/store/reducers.dart';
 
+// Test is still failing. Seems to have something to do with
+// the internationlization stuff.
+// See issue on GitHub: https://github.com/flutter/flutter/issues/22193
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    final store = Store<AppState>(rootReducer, initialState: AppState.initial());
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(store: store));
+    tester.runAsync(() async {
+      final store =
+        Store<AppState>(rootReducer, initialState: AppState.initial());
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(MyApp(store: store));
+      await tester.pumpAndSettle();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // Verify that our counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Verify that our counter has incremented.
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
+    });
   });
 }
