b7aa868be3cfcffaaa4615b47903ca313e2632df
DorianGrey <ShadowVampire@web.de>
2019-06-04 12:34:47 +0200

    Fix unit tests.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,10 +6,13 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_app_playground/counterPage.dart';
+import 'package:flutter_app_playground/i18n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_app_playground/my_app.dart';
 import 'package:flutter_app_playground/store/state.dart';
 import 'package:flutter_app_playground/store/reducers.dart';
 
@@ -18,25 +21,41 @@ import 'package:flutter_app_playground/store/reducers.dart';
 // See issue on GitHub: https://github.com/flutter/flutter/issues/22193
 
 void main() {
+  Widget createTestWidget({Widget child, Store<AppState> store}) {
+    final matApp = MaterialApp(
+        localizationsDelegates: [
+          const AppLocalizationsDelegate(),
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate
+        ],
+        supportedLocales: [
+          const Locale("en", "GB")
+        ],
+        onGenerateTitle: (context) => AppLocalizations.of(context).appHeader,
+        theme: ThemeData.light(),
+        home: child);
+    return StoreProvider(child: matApp, store: store);
+  }
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    tester.runAsync(() async {
-      final store =
+    final store =
         Store<AppState>(rootReducer, initialState: AppState.initial());
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(MyApp(store: store));
-      await tester.pumpAndSettle();
+    final widget = createTestWidget(child: CounterPage(), store: store);
 
-      // Verify that our counter starts at 0.
-      expect(find.text('0'), findsOneWidget);
-      expect(find.text('1'), findsNothing);
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(widget);
+    await tester.pumpAndSettle();
 
-      // Tap the '+' icon and trigger a frame.
-      await tester.tap(find.byIcon(Icons.add));
-      await tester.pump();
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
-      // Verify that our counter has incremented.
-      expect(find.text('0'), findsNothing);
-      expect(find.text('1'), findsOneWidget);
-    });
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
